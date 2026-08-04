02b9d5d7bb30a3098a849fe10e54fb46d5354665
TonyJ0711 <ajh.guaman@yavirac.edu.ec>
2026-08-04 17:30:23 -0500

    eliminar node_modules y backend duplicado del repositorio, corregir .gitignore

--- src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Ruta POST /api/auth/login
-router.post('/login', authController.login);
-
-module.exports = router;
--- src/routes/mallaRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMallas, createMalla } = require('../controllers/mallaController');
-
-router.get('/', getMallas);
-router.post('/', createMalla);
-
-module.exports = router;
